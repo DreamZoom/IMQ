9c29d7f9e9ce73b72f9af0f9a28d40babe53d2bf
王小龙 <1036199213@qq.com>
2015-08-05 18:05:30 +0800

    center

--- IM.Android/IM/gen/com/dzoom/im/R.java
+++ IM.Android/IM/gen/com/dzoom/im/R.java
@@ -11,42 +11,45 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black_deep=0x7f040020;
-        public static final int bottom_bar_normal_bg=0x7f04000f;
-        public static final int bottom_text_color_normal=0x7f040010;
-        public static final int btn_answer_normal=0x7f040022;
-        public static final int btn_answer_pressed=0x7f040021;
-        public static final int btn_blue_normal=0x7f040018;
-        public static final int btn_blue_pressed=0x7f040019;
-        public static final int btn_gray_normal=0x7f040015;
-        public static final int btn_gray_pressed=0x7f040017;
-        public static final int btn_gray_pressed_status=0x7f040016;
-        public static final int btn_green_noraml=0x7f040013;
-        public static final int btn_green_pressed=0x7f040014;
-        public static final int btn_login_normal=0x7f040001;
-        public static final int btn_login_pressed=0x7f040002;
-        public static final int btn_logout_normal=0x7f040003;
-        public static final int btn_logout_pressed=0x7f040004;
-        public static final int btn_pressed_green_solid=0x7f04000b;
-        public static final int btn_register_normal=0x7f040005;
-        public static final int btn_register_pressed=0x7f040006;
-        public static final int btn_white_normal=0x7f040011;
-        public static final int btn_white_pressed=0x7f040012;
-        public static final int common_bg=0x7f04000a;
-        public static final int common_bottom_bar_normal_bg=0x7f040007;
-        public static final int common_bottom_bar_selected_bg=0x7f040008;
-        public static final int common_botton_bar_blue=0x7f040009;
-        public static final int common_top_bar_blue=0x7f040000;
-        public static final int divider_list=0x7f04000c;
-        public static final int error_item_color=0x7f04000e;
-        public static final int gray_normal=0x7f04001c;
-        public static final int gray_pressed=0x7f04001b;
-        public static final int grid_state_focused=0x7f04001e;
-        public static final int grid_state_pressed=0x7f04001d;
-        public static final int main_botton_text_color=0x7f040023;
-        public static final int orange=0x7f04001a;
-        public static final int top_bar_normal_bg=0x7f04000d;
-        public static final int voip_interface_text_color=0x7f04001f;
+        public static final int app_bg_color=0x7f040000;
+        public static final int app_gray_color=0x7f040001;
+        public static final int black_deep=0x7f040022;
+        public static final int bottom_bar_normal_bg=0x7f040011;
+        public static final int bottom_text_color_normal=0x7f040012;
+        public static final int btn_answer_normal=0x7f040024;
+        public static final int btn_answer_pressed=0x7f040023;
+        public static final int btn_blue_normal=0x7f04001a;
+        public static final int btn_blue_pressed=0x7f04001b;
+        public static final int btn_gray_normal=0x7f040017;
+        public static final int btn_gray_pressed=0x7f040019;
+        public static final int btn_gray_pressed_status=0x7f040018;
+        public static final int btn_green_noraml=0x7f040015;
+        public static final int btn_green_pressed=0x7f040016;
+        public static final int btn_login_normal=0x7f040003;
+        public static final int btn_login_pressed=0x7f040004;
+        public static final int btn_logout_normal=0x7f040005;
+        public static final int btn_logout_pressed=0x7f040006;
+        public static final int btn_pressed_green_solid=0x7f04000d;
+        public static final int btn_register_normal=0x7f040007;
+        public static final int btn_register_pressed=0x7f040008;
+        public static final int btn_white_normal=0x7f040013;
+        public static final int btn_white_pressed=0x7f040014;
+        public static final int center_item_bg_color=0x7f040026;
+        public static final int common_bg=0x7f04000c;
+        public static final int common_bottom_bar_normal_bg=0x7f040009;
+        public static final int common_bottom_bar_selected_bg=0x7f04000a;
+        public static final int common_botton_bar_blue=0x7f04000b;
+        public static final int common_top_bar_blue=0x7f040002;
+        public static final int divider_list=0x7f04000e;
+        public static final int error_item_color=0x7f040010;
+        public static final int gray_normal=0x7f04001e;
+        public static final int gray_pressed=0x7f04001d;
+        public static final int grid_state_focused=0x7f040020;
+        public static final int grid_state_pressed=0x7f04001f;
+        public static final int main_botton_text_color=0x7f040025;
+        public static final int orange=0x7f04001c;
+        public static final int top_bar_normal_bg=0x7f04000f;
+        public static final int voip_interface_text_color=0x7f040021;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -57,6 +60,9 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int center_icon_margin=0x7f050002;
+        public static final int center_icon_width=0x7f050003;
+        public static final int center_item_padding_left=0x7f050004;
     }
     public static final class drawable {
         public static final int contact_list_normal=0x7f020000;
@@ -74,7 +80,7 @@ public final class R {
         public static final int unread_dot=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000d;
+        public static final int action_settings=0x7f090015;
         public static final int btn_address_list=0x7f090006;
         public static final int btn_center=0x7f09000b;
         public static final int btn_container_address_list=0x7f090005;
@@ -83,8 +89,16 @@ public final class R {
         public static final int btn_container_setting=0x7f090008;
         public static final int btn_conversation=0x7f090003;
         public static final int btn_setting=0x7f090009;
+        public static final int imageView1=0x7f09000e;
+        public static final int imageView2=0x7f090011;
+        public static final int imageView22=0x7f090013;
+        public static final int linearLayout1=0x7f09000d;
+        public static final int linearLayout2=0x7f090010;
         public static final int main_bottom=0x7f090001;
         public static final int textView1=0x7f09000c;
+        public static final int textView2=0x7f09000f;
+        public static final int textView3=0x7f090012;
+        public static final int textView32=0x7f090014;
         public static final int unread_address_number=0x7f090007;
         public static final int unread_msg_number=0x7f090004;
         public static final int vPager=0x7f090000;
